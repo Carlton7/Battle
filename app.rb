9f5082bca62c44d2048b194b0009ce89f0720fa9
carlton anand <carlton.anand@gmail.com>
2019-05-15 15:05:53 +0100

    adding additions for the entering players challenge from week 3 weekly problem

--- app.rb
+++ app.rb
@@ -5,11 +5,15 @@ require 'rspec'
 
 class Battle < Sinatra::Base
     get '/' do
-        p params
-        @player1 = params[:player1_name]
-        @player2 = params[:player1_name]
         erb(:index)
-      end
-    # start the server if ruby file executed directly
-    run! if app_file == $0
+    end
+
+    post '/player_names' do
+        @player_1_name = params[:player_1_name]
+        @player_2_name = params[:player_2_name]
+        erb(:display)
+    end
+    
 end
+
+
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,17 +2,17 @@ require_relative '../../app'
 
   feature 'testing homepage' do
     
-    scenario "checks that the homepage says Testing infrastructure working!" do
-        visit '/'
-     expect(page).to have_content "Testing infrastructure working!"
-    end 
+    # scenario "checks that the homepage says Testing infrastructure working!" do
+    #     visit '/'
+    #  expect(page).to have_content "Testing infrastructure working!"
+    # end 
 
     scenario "expects players to fill in their names" do
         visit '/'
-        fill_in 'Player 1', with: 'carlton'
-        fill_in 'Player 2', with: 'lucy'
+        fill_in :player_1_name, with: 'carlton'
+        fill_in :player_2_name, with: 'lucy'
         click_button 'Submit'
-        expect(page).to have_content "Player 1 = carlton/n Player 2 = lucy"
+        expect(page).to have_content "carlton vs. lucy"
     end 
 
   end
